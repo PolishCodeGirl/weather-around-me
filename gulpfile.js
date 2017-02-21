627736cf0e33cb10523ed755bf1661cdb7a2be29
Marzena Malachowska <malachowska.marzena@gmail.com>
2017-02-21 14:47:44 +0100

    add first version of files, mobile view of short, detail weather and map view

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+
+gulp.task("default", function() {
+    return gulp.src("scss/style.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("css"))
+});
+
+gulp.task("watch", function(){
+    gulp.watch("scss/*.scss", ["default"]);
+}) 
---
+++ js/app.js
@@ -0,0 +1,51 @@
+//document.addEventListener("DOMContentLoaded", function(){
+//
+//});
+
+$(function() {
+    function detailWeather() {
+        var more = $(".showMore");
+        var back = $(".showLess");
+        var mapBtn = $(".mapBtn");
+        var shortWeather = $(".shortWeather");
+        var longWeather = $(".longWeather");
+        var googleMap = $(".googleMap");
+        
+        more.on('click', function(){
+            shortWeather.fadeOut(function(){
+                longWeather.fadeIn();
+            });
+            //shortWeather.hide();
+            console.log("ja też działam :)");
+        });
+        
+        back.on('click', function(){
+            longWeather.fadeOut(1000, function(){
+                shortWeather.fadeIn(1000);
+            });
+            //longWeather.hide();
+            console.log("działa");
+        });
+        
+        mapBtn.on('click', function(){
+            if(!shortWeather.is(':visible')){
+                longWeather.fadeOut(function(){
+                    googleMap.fadeIn();
+                    var map = document.getElementById('map');
+                    google.maps.event.trigger(map, 'resize');
+                });
+            }
+            else if (!longWeather.is(':visible')){
+                shortWeather.fadeOut(function(){
+                    googleMap.fadeIn();
+                    var map = document.getElementById('map');
+                    google.maps.event.trigger(map, 'resize');
+                });
+            }
+            console.log("Mapa działa!!!");
+        });
+    }
+    
+    detailWeather();
+    
+});
